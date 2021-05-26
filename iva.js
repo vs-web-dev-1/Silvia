6c8f6ee2b72bfaafe943b92bde4158332fd4943a
Silvia Garcia <sgarciam@vectoritcgroup.com>
2021-05-26 09:57:18 +0200

    iva

--- iva.js
+++ iva.js
@@ -1,17 +1,15 @@
 //let ivaNormal = 21;
 let precio = 50;
 let importe1;
+let import2;
 let total1;
-
-
 
 //funcion calcula cuota con iva
 function calcularConIva(precio,ivaNormal=21) {
   let importe = precio * (ivaNormal/100);
-  //let import2 = importe * (ivaNormal/100);
+  import2 = importe * (ivaNormal/100);
   return importe;
 }
-
 
 importe1 = calcularConIva(precio);
 console.log("calcula cuota de iva:  " + importe1);
@@ -24,3 +22,6 @@ function calcularTotalIva (precio,ivaNormal=21) {
 
 total1 = calcularTotalIva(precio,ivaNormal=21);
 console.log("calcula cuota total:  " + total1);
+console.log("calcula importe2:  " + import2);
+console.log("calcula ivaNormal total:  " + ivaNormal);
+
